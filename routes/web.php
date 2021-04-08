a5bb543b4eeeda5edf80e6993a87b2e8e06eb3f4
miniyus <miniyu97@gmail.com>
2021-04-08 16:39:17 +0900

    update

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,6 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-
 $router->group(['prefix' => 'api'], function () use ($router) {
     $router->post('stocks', [
         'middleware' => 'client',
