d912a440e762507932e9c76c1ddc51de7d805895
miniyus <miniyu97@gmail.com>
2021-04-14 17:01:41 +0900

    update

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ $router->group(['prefix' => 'api', 'middleware' => 'client'], function () use ($
 
     $router->post('stocks/corp-codes', 'StockController@storeCorpCodes');
 
-    $router->get('stocks/sectors/{market}', 'StockController@getSectors');
+    $router->get('stocks/sectors/{market}', 'StockController@getSetctors');
 
     $router->get('stocks', 'StockController@index');
 
