137e681090c6b8aea998b67bf10d7e73d2aeec96
miniyus <miniyu97@gmail.com>
2021-04-26 20:32:24 +0900

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,18 +25,18 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                {{-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
-                </button>
+                </button> --}}
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                {{-- <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
 
                     </ul>
 
                     <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
+                    <ul class="navbar-nav ml-auto"> --}}
                         <!-- Authentication Links -->
                         {{-- @auth('web') --}}
                             {{-- <li class="nav-item dropdown">
@@ -72,8 +72,8 @@
                                 <a class="nav-link" href="{{ route('web.dev.login') }}">{{ __('Login') }}</a>
                             </li> --}}
                         {{-- @endauth --}}
-                    </ul>
-                </div>
+                    {{-- </ul>
+                </div> --}}
             </div>
         </nav>
 
