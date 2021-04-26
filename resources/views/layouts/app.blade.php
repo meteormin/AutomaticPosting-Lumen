130374fbbe4cea839c5fc1e96c5332d9b2d342ea
miniyus <miniyu97@gmail.com>
2021-04-26 20:38:08 +0900

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <title>{{ config('app.name', 'Lumen') }}</title>
 
 <!-- Scripts  -->
-    <script src="{{ url('/js/app.js') }}" defer></script>
+    {{-- <script src="{{ url('/js/app.js') }}" defer></script> --}}
     <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -25,55 +25,6 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
-                {{-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button> --}}
-
-                {{-- <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto"> --}}
-                        <!-- Authentication Links -->
-                        {{-- @auth('web') --}}
-                            {{-- <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::guard('web')->user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ url('/profile') }}">profile</a>
-                                    @if(Auth::guard('web')->user()->role == 1)
-                                        @if (Route::has('web.dev.register'))
-                                        <a class="dropdown-item" href="{{ route('web.dev.manage') }}">API 서버 관리</a>
-                                        @endif
-                                        @if (Route::has('client'))
-                                        <a class="dropdown-item" href="{{ route('client') }}">API 클라이언트 관리</a>
-                                        @endif
-                                        <a class="dropdown-item" href="{{ url('http://125.7.128.24')}}" target="_blank">관리자 사이트</a>
-                                    @endif
-                                    <a class="dropdown-item" href="{{ route('web.dev.logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('web.dev.logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-
-
-                                </div>
-                            </li>
-                        @else
-
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('web.dev.login') }}">{{ __('Login') }}</a>
-                            </li> --}}
-                        {{-- @endauth --}}
-                    {{-- </ul>
-                </div> --}}
             </div>
         </nav>
 
