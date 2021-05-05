3386e009778bd0a196bd6bdd99b51eb2521076fc
miniyus <miniyu97@gmail.com>
2021-05-05 11:36:53 +0900

    update

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -8,7 +8,7 @@
 <div class="container">
     <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
-            <center>><h3>Not Found Page</h3></center
+            <center><h3>Not Found Page</h3></center>
         </div>
     </div>
 </div>
