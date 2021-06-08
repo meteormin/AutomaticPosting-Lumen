8b880c5f8ae260860297b9163a2e7f1a67f77f33
miniyus <miniyu97@gmail.com>
2021-06-08 11:37:42 +0900

    update: 0.6.1 create updatestockinfo command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,6 +19,7 @@ class Kernel extends ConsoleKernel
         \App\Console\Commands\AutoPost::class,
         \App\Console\Commands\MediumPost::class,
         \App\Console\Commands\WpPost::class,
+        \App\Console\Commands\UpdateStockInfo::class
     ];
 
     /**
