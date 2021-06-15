d6287ef26e0f726ec4b10680fb3e2e6ae433f2b0
miniyus <miniyu97@gmail.com>
2021-06-15 20:49:37 +0900

    update: 0.6.2 treemap 기준 항목 변경

--- app/Http/Controllers/InfoGraphics/MainController.php
+++ app/Http/Controllers/InfoGraphics/MainController.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\Controller;
 use App\Services\Infographics\Service;
 use Illuminate\Http\Request;
 use Illuminate\Contracts\Filesystem\FileNotFoundException;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\View\View;
 use JsonMapper_Exception;
 use Laravel\Lumen\Application;
@@ -30,7 +32,22 @@ class MainController extends Controller
      */
     public function index()
     {
-        return view('sb-admin.content');
+        $disk = Storage::disk('local');
+        $files = $disk->files("kiwoom/theme/");
+        $max = 0;
+        $recent = 0;
+
+        foreach ($files as $f) {
+            if ($max < $disk->lastModified($f)) {
+                $max = $disk->lastModified($f);
+                $recent = explode('_', $f)[1];
+                $recent = explode('.', $recent)[0];
+            }
+        }
+
+        $treemap = $this->service->getTreeMapChart('theme', $recent);
+        $barchart = $this->service->getBarChart('theme', $recent);
+        return view('sb-admin.content', ['google_chart' => ['treemap' => $treemap, 'bar' => $barchart]]);
     }
 
     /**
